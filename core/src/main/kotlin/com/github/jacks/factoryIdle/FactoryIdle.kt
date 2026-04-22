4f0ee1cee3789760a66a1b69728caac457e97f90
Tyler <tdavis1993@gmail.com>
2026-04-22 18:30:41 +0900

    init commit

---
+++ core/src/main/kotlin/com/github/jacks/factoryIdle/FactoryIdle.kt
@@ -0,0 +1,6 @@
+package com.github.jacks.factoryIdle
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class FactoryIdle : ApplicationAdapter()
